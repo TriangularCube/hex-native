03ad9d7dcd193156c998983bce65e00b5b1129fe
Michael Liu <michael.liu0@gmail.com>
2019-07-24 21:20:57 -0700

    Testing tabbed navigators

--- App.js
+++ App.js
@@ -1,19 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import { StyleSheet, View, SafeAreaView, StatusBar, Platform } from 'react-native';
+import {Appbar, Text, Provider as PaperProvider} from 'react-native-paper';
+import {createAppContainer, createBottomTabNavigator} from "react-navigation";
+import WindowGuard from "react-native-window-guard";
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        // justifyContent: 'center'
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+    },
 });
+
+function Screen1() {
+    return (
+        <View style={styles.container}>
+            <Text>{Platform.OS}</Text>
+        </View>
+    );
+}
+
+function Screen2(){
+    return(
+        <View style={styles.container}>
+            <Text>Test</Text>
+        </View>
+    )
+}
+
+const bottomNavigator = createBottomTabNavigator({
+    Home: {
+        screen: Screen1
+    },
+    User: {
+        screen: Screen2
+    }
+});
+
+const Container = createAppContainer( bottomNavigator );
+
+export default class App extends React.Component{
+    componentDidMount() {
+        WindowGuard.requestWindowInsets();
+    }
+
+    render(){
+        return(
+            <>
+                <PaperProvider>
+                    <WindowGuard applyInsets={WindowGuard.all}>
+                        <Container />
+                    </WindowGuard>
+                </PaperProvider>
+            </>
+        );
+    }
+}
